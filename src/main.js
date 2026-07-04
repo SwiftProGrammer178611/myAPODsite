e11a344736d62965cac07b7013eda4eb04d7285a
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-04 02:19:25 -0400

    add deploy workflow

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import './style.css'
 
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>My NASA APOD Site</h1>
     <p>Welcome to my Hack Club mission project!</p>
   </div>
 `
+const date = document.querySelector("#datepicker").value;
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    let media;
+
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}"/>`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
+
+    document.querySelector("#app").innerHTML = `
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+  });
